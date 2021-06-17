60a2eef36e7be4197db2180b871f1ba69aa95343
AlanSoto31 <soto.alan@hotmail.com>
2021-06-17 16:35:12 -0500

    Fetch data from the API

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const AnimeList = () => (
-  <div>
-    <h1>AnimeList</h1>
-  </div>
-);
+const AnimeList = () => {
+  const [listGames, setListGames] = useState([]);
+
+  const fetchGames = async () => {
+    const games = await fetch('https://frozen-shelf-35879.herokuapp.com/https://www.giantbomb.com/api/games/?api_key=a49d1e3f0d18a6120a595dd1d2b6d2f4c8278c5b&format=json&limit=50&field_list=image,description,deck,name&filter=platforms:3').then((res) => res.json());
+    setListGames(games.results);
+  };
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
+  const clickEvent = () => {
+    console.log(listGames);
+  };
+
+  const items = listGames.map((game) => <li key={game.id}>{game.name}</li>);
+
+  return (
+    <>
+      <button type="button" onClick={clickEvent}>print list</button>
+      <ul>
+        {items}
+      </ul>
+    </>
+  );
+};
 
 export default AnimeList;
